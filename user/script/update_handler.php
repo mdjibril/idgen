60ed7605a4bf430cb3f3833dcaffcf0bbebe9c6b
mdjibril <muhammadjibrildauda@gmail.com>
2023-07-17 17:25:43 +0100

    script to update student record

--- user/script/update_handler.php
+++ user/script/update_handler.php
@@ -1,6 +1,7 @@
 <?php 
     session_start();
-    require 'connection.php';
+    require '../../connection.php';
+    // define ('SITE_ROOT', realpath(dirname(__FILE__)));
 
     if (isset($_POST['update'])) {
         $fullname = $_POST['fullname'];
@@ -8,7 +9,22 @@
         $gender = $_POST['gender'];
         $level = $_POST['level'];
 
-        $pasport = $_POST['passport']['name'];
+        $passport = $_FILES['passport']['name'];
+        $extension = explode(".", $passport);
+        $newfilename = $matric.'.'.end($extension);
+        $temp_name = $_FILES['passport']['tmp_name'];
+        
+        $path = "../../passport/".$newfilename;
+        $sql = "UPDATE `user` SET `fullname` = '$fullname', `matric_number` = '$matric', `gender` = '$gender', `passport` = '$path' WHERE `matric_number` = '$matric'";
+
+        $uploadtodb = move_uploaded_file($temp_name, $path);
+
+        $update = mysqli_query($conn, $sql);
+        if ($update && $uploadtodb) {
+            echo "Record and passport has been updated";
+        }else {
+            echo "error";
+        }
 
     }
 ?>
